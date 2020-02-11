ba0e08ea2166b89e364e23355701894369021d1a
rmndii <rmndii@live.jp>
2020-02-11 22:11:04 +0900

    Add files via upload

--- js/style.js
+++ js/style.js
@@ -47,16 +47,8 @@ $(function ()
 
 $(function ()
 {
-    $('.Toggle').click(function ()
+    $('#nav-toggle').on('click', function ()
     {
-        $(this).toggleClass('active');
-
-        if ($(this).hasClass('active'))
-        {
-            $('.NavMenu').addClass('active');　 //クラスを付与
-        } else
-        {
-            $('.NavMenu').removeClass('active'); //クラスを外す
-        }
+        $('body').toggleClass('open');
     });
 });
