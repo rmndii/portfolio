146f74c0602c09611432d98799c1d1483230eff7
rmndii <rmndii@live.jp>
2020-02-11 21:52:37 +0900

    Add files via upload

--- js/style.js
+++ js/style.js
@@ -43,3 +43,20 @@ $(function ()
         }
     });
 });
+
+
+$(function ()
+{
+    $('.Toggle').click(function ()
+    {
+        $(this).toggleClass('active');
+
+        if ($(this).hasClass('active'))
+        {
+            $('.NavMenu').addClass('active');　 //クラスを付与
+        } else
+        {
+            $('.NavMenu').removeClass('active'); //クラスを外す
+        }
+    });
+});
