665358aae27c2eb141787dc57e9aa6c599545d90
rmndii <rmndii@live.jp>
2020-02-11 23:19:14 +0900

    Add files via upload

--- js/style.js
+++ js/style.js
@@ -20,5 +20,53 @@ $(function (){
         return false;
     });
 
+    $(function ()
+    {
+        var headerHight = 80; //ヘッダの高さ
+        $('a[href^=#]').click(function ()
+        {
+            var href = $(this).attr("href");
+            var target = $(href == "#" || href == "" ? 'html' : href);
+            var position = target.offset().top - headerHight; //ヘッダの高さ分位置をずらす
+            $("html, body").animate({ scrollTop: position }, 550, "swing");　//この数値は移動スピード
+            return false;
+        });
+    });
 
+
+    var navPos = jQuery('header').offset().top; // グローバルメニューの位置
+    var navHeight = jQuery('header').outerHeight(); // グローバルメニューの高さ
+    jQuery(window).on('scroll', function ()
+    {
+        if (jQuery(this).scrollTop() > navPos)
+        {
+            jQuery('body').css('padding-top', 0);
+            jQuery('header').addClass('fixed');
+        } else
+        {
+            jQuery('body').css('padding-top', 0);
+            jQuery('header').removeClass('fixed');
+        }
+    });
+
+    /*
+    * ハンバーガー
+    */
+    $(function ()
+    {
+        $(".burger-icon").on("click", function ()
+        {
+            if ($(this).hasClass("is-burger-nav-open"))
+            {
+                $(this).removeClass("is-burger-nav-open");
+                $(".burger-nav").removeClass("open");
+            } else
+            {
+                $(this).addClass("is-burger-nav-open");
+                $(".burger-nav").addClass("open");
+            }
+        });
+    });
+
+    
 });
