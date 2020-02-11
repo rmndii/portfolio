9f03895ea56fffda43ceeb22fc9cf73b27a3b92c
rmndii <rmndii@live.jp>
2020-02-11 22:30:05 +0900

    Add files via upload

--- js/style.js
+++ js/style.js
@@ -20,24 +20,5 @@ $(function (){
         return false;
     });
 
-    var $win = $(window),
-        $main = $('main'),
-        $nav = $('nav'),
-        navHeight = $nav.outerHeight(),
-        navPos = $nav.offset().top,
-        fixedClass = 'transform';
 
-    $win.on('load scroll', function ()
-    {
-        var value = $(this).scrollTop();
-        if (value > navPos)
-        {
-            $nav.addClass(fixedClass);
-            $main.css('margin-top', navHeight);
-        } else
-        {
-            $nav.removeClass(fixedClass);
-            $main.css('margin-top', '0');
-        }
-    });
 });
